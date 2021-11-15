872b6499c61cf97ec6b6ffa079a3e1bc154f6d5d
Wesley Handy <whandy@costar.com>
2021-11-15 11:43:41 -0500

    Remove dist folder, update .gitingore

--- dist/utils/url.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.completeUrl = void 0;
-function completeUrl(baseUrl, path, query) {
-    const searchParams = new URLSearchParams(query);
-    return new URL(`${path}?${searchParams.toString()}`, baseUrl).toString();
-}
-exports.completeUrl = completeUrl;
-//# sourceMappingURL=url.js.map
